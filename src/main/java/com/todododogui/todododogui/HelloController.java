acb33a1cecd30ed849a649e01efd9c2ae1a85320
Juno-ko <juno-koo@pm.me>
2023-10-30 15:38:58 +0200

    Now adding one task works. Need to make ArrayList next.

--- src/main/java/com/todododogui/todododogui/HelloController.java
+++ src/main/java/com/todododogui/todododogui/HelloController.java
@@ -1,14 +1,41 @@
 package com.todododogui.todododogui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
+    public TextField newTask;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Successfully added new task");
+    private Label showTasks;
+    @FXML
+    protected void submitClicked() {
+        String taskToBeAdded = newTask.getText();
+        showTasks.setText("1. " + taskToBeAdded);
     }
+    static void printTasks(ArrayList<String> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+    static void addTask(ArrayList<String> tasks) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("\nAdd new task:");
+        String task = myObj.nextLine();
+        tasks.add(task);
+    }
+    static void markTaskDone(ArrayList<String> tasks) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("\nEnter tasks number to mark as done:\n");
+        int taskNumber = myObj.nextInt();
+        if (taskNumber >= 1 && taskNumber <= tasks.size()) {
+            tasks.remove(taskNumber - 1);
+        }
+    }
+
 }
