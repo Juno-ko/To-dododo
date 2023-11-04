ce9cfef64c1cce8977b3d93475cf62ee217d5fed
Juno-ko <juno-koo@pm.me>
2023-11-04 11:58:18 +0200

    Making GUI more mature

--- src/main/java/com/todododogui/todododogui/HelloApplication.java
+++ src/main/java/com/todododogui/todododogui/HelloApplication.java
@@ -1,23 +1,47 @@
 package com.todododogui.todododogui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("To-dododo");
-        stage.setScene(scene);
-        stage.show();
-    }
+
+    private ObservableList<String> tasks = FXCollections.observableArrayList();
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("To-Do List App");
+
+        ListView<String> taskListView = new ListView<>(tasks);
+        TextField newTaskField = new TextField();
+        Button addButton = new Button("Add");
+
+        addButton.setOnAction(event -> {
+            String newTask = newTaskField.getText();
+            if (!newTask.isEmpty()) {
+                tasks.add(newTask);
+                newTaskField.clear();
+            }
+        });
+
+        HBox inputBox = new HBox(newTaskField, addButton);
+        HBox.setHgrow(newTaskField, Priority.ALWAYS);
+        newTaskField.setMaxWidth(Double.MAX_VALUE);
+
+        VBox root = new VBox(taskListView, inputBox);
+        Scene scene = new Scene(root, 300, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
