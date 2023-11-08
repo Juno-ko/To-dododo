d264255f402938cac9453a9cde055ae0a589bebe
Juno-ko <juno-koo@pm.me>
2023-11-08 16:22:00 +0200

    Add button on right position

--- src/main/java/com/todododogui/todododogui/HelloApplication.java
+++ src/main/java/com/todododogui/todododogui/HelloApplication.java
@@ -7,13 +7,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
 public class HelloApplication extends Application {
 
-    private ObservableList<TaskItem> tasks = FXCollections.observableArrayList();
+    private final ObservableList<TaskItem> tasks = FXCollections.observableArrayList();
 
     public static void main(String[] args) {
         launch(args);
@@ -23,25 +24,7 @@ public class HelloApplication extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("To-dododo");
 
-        ListView<TaskItem> taskListView = new ListView<>(tasks);
-        taskListView.setCellFactory(new Callback<ListView<TaskItem>, ListCell<TaskItem>>() {
-            @Override
-            public ListCell<TaskItem> call(ListView<TaskItem> param) {
-                return new ListCell<TaskItem>() {
-                    @Override
-                    protected void updateItem(TaskItem item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item != null) {
-                            setText(item.getTaskName());
-                            setGraphic(createDeleteButton(item));
-                        } else {
-                            setText("");
-                            setGraphic(null);
-                        }
-                    }
-                };
-            }
-        });
+        ListView<TaskItem> taskListView = getTaskItemListView();
 
         TextField newTaskField = new TextField();
         Button addButton = new Button("Add");
@@ -64,35 +47,52 @@ public class HelloApplication extends Application {
             }
         });
 
-        VBox root = new VBox(taskListView, new HBox(newTaskField, addButton));
+        HBox inputBox = new HBox(newTaskField, addButton);
+        HBox.setHgrow(newTaskField, Priority.ALWAYS);
+        HBox.setHgrow(addButton, Priority.ALWAYS);
+        newTaskField.setMaxWidth(Double.MAX_VALUE);
+
+        VBox root = new VBox(taskListView, inputBox);
         Scene scene = new Scene(root, 300, 400);
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private class TaskItem {
-        private String taskName;
+    private ListView<TaskItem> getTaskItemListView() {
+        ListView<TaskItem> taskListView = new ListView<>(tasks);
+        taskListView.setCellFactory(new Callback<>() {
+            @Override
+            public ListCell<TaskItem> call(ListView<TaskItem> param) {
+                return new ListCell<>() {
+                    @Override
+                    protected void updateItem(TaskItem item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item != null) {
+                            setText(item.taskName());
+                            setGraphic(createDeleteButton(item));
+                        } else {
+                            setText("");
+                            setGraphic(null);
+                        }
+                    }
+                };
+            }
+        });
+        return taskListView;
+    }
 
-        public TaskItem(String taskName) {
-            this.taskName = taskName;
-        }
-
-        public String getTaskName() {
-            return taskName;
-        }
+    private record TaskItem(String taskName) {
 
         @Override
-        public String toString() {
-            return taskName;
+            public String toString() {
+                return taskName;
+            }
         }
-    }
 
     private Button createDeleteButton(TaskItem item) {
         Button deleteButton = new Button("X");
-        deleteButton.setOnAction(event -> {
-            tasks.remove(item);
-        });
+        deleteButton.setOnAction(event -> tasks.remove(item));
         return deleteButton;
     }
 }
